5b9703de94e7b31a1f78469d9252d2a668aef884
dreamPerson <111998230+dreamPerson0106@users.noreply.github.com>
2023-03-24 18:17:48 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import "./App.css";
+
+import Driveshare from "./driveshare";
+
+function App() {
+  return (
+    // <div className="container-bg">
+    //   <div className="container-logo">
+    //     <img src={logo} alt="logo" />
+    //   </div>
+
+    //   <div className="container-body">
+    //     <div className="container-heading">
+    //       <h1>Download our</h1>
+    //       <h1>
+    //         <span>driveshare</span> app now
+    //       </h1>
+    //     </div>
+
+    //     <div className="actions-container">
+    //       <div className="actions-appstore">
+    //         <img src={appstore} alt="app-store" />
+    //       </div>
+    //       <div className="actions-playstore">
+    //         <img src={playstore} alt="play-store" />
+    //       </div>
+    //     </div>
+
+    //     <div className="tokenomic-container">
+    //       <div className="tokenomic-title">Tokenomic</div>
+    //       <div className="tokenomic-section">
+    //         <div className="tokenomic-body">
+    //           <div className="tokenomic-section1">
+    //             <div className="tokenomic-subtitle1">Total Supply</div>
+    //             <div className="tokenomic-subtitle2">1000, 000, 000</div>
+    //           </div>
+    //           <div className="tokenomic-pipe"></div>
+    //           <div className="tokenomic-section2">
+    //             <div className="tokenomic-subtitle1">Buy/Sell Tax:</div>
+    //             <div className="tokenomic-subtitle2">5/5</div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <div className="ca-id">
+    //       CA: <span>0x59895c2192d8c30DA145156a70C92396e96a58af</span>
+    //     </div>
+    //   </div>
+
+    //   <div className="social-media-footer">
+    //     <div className="social-media">
+    //       <div className="twitter-container">
+    //         <div className="twitter-img">
+    //           <img src={twitter} alt="twitter" />
+    //         </div>
+    //         <div className="twitter-name">Twitter</div>
+    //       </div>
+
+    //       <div className="telegram-container">
+    //         <div className="telegram-img">
+    //           <img src={telegram} alt="telegram" />
+    //         </div>
+    //         <div className="telegram-name">Telegram</div>
+    //       </div>
+
+    //       <div className="medium-container">
+    //         <div className="medium-img">
+    //           <img src={medium} alt="medium" />
+    //         </div>
+    //         <div className="medium-name">Medium</div>
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="car-container">
+    //     <img src={car} alt="car" />
+    //   </div>
+    // </div>
+
+    <Driveshare />
+  );
+}
+
+export default App;
---
+++ src/driveshare.jsx
@@ -0,0 +1,203 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../src/assets/images/Logo.svg";
+import playstore from "../src/assets/images/playstore.png";
+import appstore from "../src/assets/images/appstore.png";
+import checks from "../src/assets/images/checks.png";
+import cloud from "../src/assets/images/cloud.png";
+import cloud1 from "../src/assets/images/cloud1.png";
+import rivershare from "../src/assets/images/rivershare.png";
+import twitter from "../src/assets/images/twitter.svg";
+import telegram from "../src/assets/images/telegram.svg";
+import medium from "../src/assets/images/medium.svg";
+import car from "../src/assets/images/car.png";
+import driveshare from "../src/assets/images/driveshare_phone.png";
+import "../src/assets/css/custom.css";
+
+import { useState } from "react";
+
+const Driveshare = () => {
+  const [OpenModal, SetOpenModal] = useState(false);
+  return (
+    <div className="back">
+      <div className="row justify-content-start">
+        <div className="col-md-4">
+          <img
+            src={logo}
+            alt="logo"
+            className="position-absolute ms-5 mt-4"
+            height={110}
+            width={150}
+          />
+          <div className="d-flex justify-content-end ms-5">
+            <img
+              src={cloud}
+              alt="cloud"
+              className="position-absolute mt-5 ps-5"
+              height={68}
+            />
+          </div>
+        </div>
+        <div className="col-md-8">
+          {/*<div className="d-flex justify-content-end">
+  <img src={checks} alt="checks" />*/}
+
+          <div className="d-flex mt-0 me-0 justify-content-end">
+            <div className="d-flex justify-content-end">
+              <div className="d-flex align-items-center mt-5 me-3 download">
+                <h5 className="text-uppercase ">
+                  Download our{" "}
+                  <span className="driveshare-color">driveshare</span> app
+                </h5>
+              </div>
+              <img
+                src={checks}
+                alt="checks"
+                className="checks position-fixed"
+              />
+              <div
+                data-bs-target="#staticBackdrop"
+                onClick={() => {
+                  SetOpenModal(true);
+                }}
+              >
+                <img
+                  src={appstore}
+                  alt="playstore"
+                  height={60}
+                  width={182}
+                  className=" mt-5 position-relative cursor"
+                />
+              </div>
+              <div
+                onClick={() => {
+                  SetOpenModal(true);
+                }}
+              >
+                <img
+                  src={playstore}
+                  alt="appstore"
+                  height={60}
+                  width={182}
+                  className="me-5 ms-3 mt-5 position-relative cursor"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="d-flex justify-content-end">
+            <img
+              src={driveshare}
+              alt="main"
+              height={"75%"}
+              width={"auto"}
+              className="phone-img me-5"
+            />
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="d-flex justify-content-start rivershare">
+            <img src={rivershare} alt="rivershare" />
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="cloud-img">
+            <img src={cloud1} alt="cloud1" height={"30%"} width={"auto"} />
+          </div>
+          <div className="tokenomics">
+            <h5 className="text-uppercase driveshare-color align-items-center">
+              Tokenomics
+            </h5>
+          </div>
+
+          <div className="d-flex tokonomics-content">
+            <div className="tokonomi_text align-items-center">
+              <p className="mb-0 text-center">Total Supply</p>
+              <p className="driveshare-color">1000,000,000</p>
+            </div>
+            <div className="vr my-3 ms-3"></div>
+
+            <div className="tokonomi_text align-items-center">
+              <p className="mb-0">Buy/Sell Tax</p>
+              <p className="driveshare-color ">5/5</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="ca-text">
+            <p> CA: &nbsp; &nbsp; &nbsp;Coming Soon</p>
+          </div>
+        </div>
+      </div>
+      <div className="row ">
+        <div className="col-md-9 offset-md-3">
+          <div className="driveshare-footer d-flex">
+            <div className="driveshare-icon ms-5 ">
+              <img src={twitter} alt="tweet" />
+              <a
+                href="https://twitter.com/driversharedao"
+                target={"_blank"}
+                rel="noreferrer"
+                className="text-decoration-none text-dark"
+              >
+                <p>Twitter</p>
+              </a>
+            </div>
+            <div className="driveshare-icon ms-5">
+              <img src={telegram} alt="tweet" />
+              <p>Telegram</p>
+            </div>
+            <div className="driveshare-icon ms-5">
+              <img src={medium} alt="tweet" />
+              <a
+                href="https://medium.com/@drivesharedao"
+                target={"_blank"}
+                rel="noreferrer"
+                className="text-decoration-none text-dark"
+              >
+                <p>Medium</p>
+              </a>
+            </div>
+          </div>
+          <div className="car-footer">
+            <img src={car} alt="car" className="car-size" />
+          </div>
+        </div>
+      </div>
+      {OpenModal && (
+        <div
+          className="modal  show modal-lg position-absolute d-block"
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <div className="modal-dialog modal-dialog-centered ">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => {
+                    SetOpenModal(false);
+                  }}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="d-flex justify-content-center">
+                  <img src={logo} alt="logo-modal" />
+                </div>
+                <h2 className="driveshare-color text-center mt-3">
+                  Comming Soon
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Driveshare;
